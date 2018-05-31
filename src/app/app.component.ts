422db36f1b00049e660de1766261f2da8a269c08
anandHumesis <anand@humesis.com>
2018-05-31 12:15:39 -0700

    Remove the Stock index page from starter app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Getting Started with Angular 6';
+  subTitle = 'Angular 6 CRUD application using json-server as mocked backend API';
 }
